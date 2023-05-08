14eafffa52fede7083d3c9d7ce90c2a314380a6a
ycardosogl <ycardoso050@gmail.com>
2023-05-08 13:53:53 -0400

    8/05

--- App.js
+++ App.js
@@ -1,57 +1,63 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View, TextInput } from 'react-native';
+import { FlatList, StyleSheet, Text, View, TextInput,TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-  const listaInicial = [{ nome: 'nome', preco: 'preco' }];
-  const [listaMateriais, definirListaMateriais] = useState(listaInicial);
-  
-  
+  const [nome, setNome] = useState('');
+  const [preco, setPreco] = useState('');
+  const [listaCompras, setListaCompras] = useState([]);
 
-  const adicionarPreco = (preco) => {
-    const novoItem =[{ nome: nome, preco: preco }];
-    definirListaMateriais([...listaMateriais, novoItem]);
+  const adicionarItem = () => {
+    if (nome && preco) {
+      const novoItem = { nome, preco };
+      setListaCompras([...listaCompras, novoItem]);
+      setNome('');
+      setPreco('');
+    }
   };
 
-  const renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text>{item.nome  !== '' ? ` ${item.nome}` : ''}</Text>
-      <Text>{item.preco !== '' ? `R$ ${item.preco}` : ''}</Text>
+  const excluirItem = (index) => {
+    const novaListaCompras = [...listaCompras];
+    novaListaCompras.splice(index, 1);
+    setListaCompras(novaListaCompras);
+  };
+
+  const ItemLista = ({ item, index }) => (
+    <View style={styles.itemLista}>
+      <Text>{item.nome}</Text>
+      <Text>R${item.preco}</Text>
+      <TouchableOpacity style={styles.botaoExcluir} onPress={() => excluirItem(index)}>
+        <Text style={styles.botaoExcluirTexto}>Excluir</Text>
+      </TouchableOpacity>
     </View>
   );
-
   return (
     <View style={styles.container}>
-      <Text>APP - Lista de Material Escolar</Text>
-      <FlatList
-        data={listaMateriais}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => index.toString()}
-        numColumns={2}
-      />
-      <TextInput
-        placeholder={'Inserir Novo Item'}
-        onSubmitEditing={({ nativeEvent }) => {
-          const ultimoItem = listaMateriais[listaMateriais.length - 1];
-          if (ultimoItem.preco !== '') {
-            definirListaMateriais(
-              listaMateriais.concat({ nome: nativeEvent.text, preco: '' })
-            );
-          }
-        }}
-      />
-
-      <TextInput
-        placeholder={'Inserir Novo Preço'}
-        onSubmitEditing={({ nativeEvent }) => {
-          if (ultimoItem.nome !== '') {
-            adicionarPreco(nativeEvent.text);
-          }
-        }}
-      />
-
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Lista de Compras</Text>
+      <View style={styles.camposContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do item"
+          value={nome}
+          onChangeText={setNome}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Preço"
+          value={preco}
+          onChangeText={setPreco}
+          keyboardType="numeric"
+        />
+      </View>
+      <TouchableOpacity style={styles.botao} onPress={adicionarItem}>
+        <Text style={styles.botaoTexto}>Adicionar</Text>
+      </TouchableOpacity>
+      <View style={styles.listaContainer}>
+        {listaCompras.map((item, index) => (
+          <ItemLista key={index} item={item} index={index} />
+        ))}
+      </View>
     </View>
   );
 }
@@ -59,18 +65,57 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d3d3d3',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-  item: {
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  camposContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginRight: 10
+  },
+  botao: {
+    backgroundColor: '#007aff',
+    padding: 10,
+    borderRadius: 5,
+    alignSelf: 'stretch',
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    shadowOffset: {
+      width: 0,
+      height: 2
+    }
+  },
+  botaoTexto: {
+    color: '#fff',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  listaContainer: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 20
+  },
+  itemLista: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
-    width: '100%',
-  },
+    paddingVertical: 10
+  }
 });
